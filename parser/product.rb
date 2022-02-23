1eb94d320d5b493f6bf144fb684cd0b41c215b8f
Vlad <84670334+VladSatsukevich@users.noreply.github.com>
2022-02-23 16:26:46 +0300

    Update product.rb

--- parser/product.rb
+++ parser/product.rb
@@ -1,5 +1,3 @@
-require 'csv'
-
 class Product
 
   attr_accessor :file_name, :name, :price, :image
@@ -10,19 +8,7 @@ class Product
       @price = price
       @image = image
     end
-   
-    def self.csv_create (file_name)
-      CSV.open(file_name + ".csv", 'w') do |csv|
-      csv << ["Name", "Price", "Image"]
-      puts "File #{file_name} was created!"
-      end
-    end
-
-    def self.csv_add (file_name, name, price, image)
-      CSV.open(file_name + ".csv", 'a') do |csv|
-      csv << [name, price, image]
-      end
-    end
 
 end
+
 
