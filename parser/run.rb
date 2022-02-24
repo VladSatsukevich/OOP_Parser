e301f11f5b2ee03065ba13bb8e5a1d79c0e1650e
Vlad <84670334+VladSatsukevich@users.noreply.github.com>
2022-02-24 10:55:18 +0300

    Update run.rb

--- parser/run.rb
+++ parser/run.rb
@@ -4,5 +4,5 @@ require_relative 'product'
 require_relative 'parsing'
 
 parameters = YAML.load_file('parameters.yaml')
-CSV_modules.csv_create(parameters['file'])
+Product.csv_create(parameters['file'])
 Parsing.parse_category(parameters['file'], parameters['link'])
