9fcea4cb1a1385df145e282ce34d28353f037041
Vlad <84670334+VladSatsukevich@users.noreply.github.com>
2022-02-24 17:07:22 +0300

    Update csv_modules.rb

--- parser/csv_modules.rb
+++ parser/csv_modules.rb
@@ -1,13 +1,13 @@
 require 'csv'
 
-module CSV_modules
-    def csv_add (file_name, name, price, image)
+class Csv_modules
+    def self.csv_add (file_name, name, price, image)
         CSV.open(file_name + ".csv", 'a') do |csv|
         csv << [name, price, image]
         end
     end
         
-    def csv_create (file_name)
+    def self.csv_create (file_name)
         CSV.open(file_name + ".csv", 'w') do |csv|
         csv << ["Name", "Price", "Image"]
         puts "File #{file_name} was created!"
