8ea5878ec8b737aadb0e7c8a300fa9e7833edee8
Vlad <84670334+VladSatsukevich@users.noreply.github.com>
2022-02-24 12:46:06 +0300

    Update product.rb

--- parser/product.rb
+++ parser/product.rb
@@ -16,8 +16,7 @@ class Product
         CSV.open(file_name + ".csv", 'w') do |csv| csv << ["Name", "Price", "Image"] 
         end
       end
-  
-    protected
+ 
       def self.csv_add (file_name, name, price, image)
         CSV.open(file_name + ".csv", 'a') do |csv| csv << [name, price, image] 
         end
