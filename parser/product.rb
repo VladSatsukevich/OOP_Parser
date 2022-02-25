ae9a0e845b43d5ea1641b279150a540795814590
Vlad <84670334+VladSatsukevich@users.noreply.github.com>
2022-02-25 15:03:38 +0300

    Update product.rb

--- parser/product.rb
+++ parser/product.rb
@@ -2,20 +2,22 @@ require_relative 'csv_modules'
 
 class Product
 
+  @@name
+  @@price
+  @@image
+  @@file_name
+
   attr_accessor :name, :price, :image
-  
-  def initialize (name, price, image)
+
+  def initialize(name, price, image)
     @name = name
     @price = price
     @image = image
-  
   end
-  
+
   private
-  def self.save 
-    Csv_modules.csv_add(name, price, image)
-  end
+    def Product.save(name, price, image, file_name)
+      Csv_modules.csv_add(name, price, image, file_name)
+    end
   
 end
-
-
